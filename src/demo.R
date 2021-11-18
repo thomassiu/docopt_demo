7ac981ad3d4be1c87b8623a3e1be5218bdf6926d
Thomas Siu <thomas.siu@gmail.com>
2021-11-18 11:56:31 -0800

    Added main function

--- src/demo.R
+++ src/demo.R
@@ -12,6 +12,12 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+
+main <- function() {
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg4)
+}
+
+main()
